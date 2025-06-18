6812f3bc74ade085ffd02ce5d5d7a3d45277f910
saro-dev <codersaro@gmail.com>
2025-06-18 15:04:37 +0530

    added senior junior tag limitations, hided @all for jr., maded responsive

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,46 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png'],
+      manifest: {
+        name: 'Rapid Collaborate',
+        short_name: 'RC App',
+        start_url: '/',
+        display: 'standalone',
+        background_color: '#ffffff',
+        theme_color: '#6A0572',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          },
+          {
+            src: 'apple-touch-icon.png',
+            sizes: '180x180',
+            type: 'image/png'
+          }
+        ]
+      },
+      devOptions: {
+        enabled: true
+      }
+    })
+  ],
   server: {
-    host: '0.0.0.0',  // listen on all interfaces
-    port: 5174,       // or your desired port
+    host: '0.0.0.0',
+    port: 5174,
   },
   // base: '/ccp/',
   define: {
